972b8786b7f6dd3c1309e315dac81f268e49c43b
Martha Edwards <martha_edwards@brown.edu>
2017-08-14 00:12:02 -0700

    Additional files and changes for heroku

--- lackingcontextweb/wsgi.py
+++ lackingcontextweb/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lackingcontextweb.settings")
 
 application = get_wsgi_application()
+
+# for Heroku deployment
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
